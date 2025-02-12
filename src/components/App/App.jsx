cfdf5b941fe11ef6927817503167fc32b246bc98
gjone124 <gjone124@gmail.com>
2025-02-11 19:41:19 -0500

    configuring files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="app">Hello</div>;
+  return (
+    <div className="page">
+      <div className="page__content">Hello</div>
+    </div>
+  );
 }
 
 export default App;
