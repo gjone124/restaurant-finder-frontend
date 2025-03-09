21fcc666cf3602d0048a10ee525c42fad083cdbe
gjone124 <gjone124@gmail.com>
2025-03-08 23:16:40 -0500

    GitHub Pages modified

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/restaurant-finder-frontend/",
   plugins: [react()],
   server: {
     port: 4000,
